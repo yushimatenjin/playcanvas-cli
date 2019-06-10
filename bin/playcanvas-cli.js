a8995446f12fed9fd927f52146117ff37504fae1
yushimatenjin <ryuto.haga@gmail.com>
2019-06-10 11:58:52 +0900

    add scripts

--- bin/playcanvas-cli.js
+++ bin/playcanvas-cli.js
@@ -1,4 +1,21 @@
 #!/usr/bin/env node
-import spawn from "cross-spawn";
+import { create } from "../scripts/commands";
 const args = process.argv.slice(2);
-console.log(args);
+
+const scriptIndex = args.findIndex(x => x === "create" || x === "init");
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+switch (script) {
+  case "create": {
+    const projectName = args[1];
+    create(projectName);
+
+    break;
+  }
+  case "init": {
+    break;
+  }
+  default: {
+    console.log(`Unknown script ${script} .`);
+    break;
+  }
+}
---
+++ scripts/create.js
@@ -0,0 +1,7 @@
+import inquirer from "inquirer";
+import spawn from "cross-spawn";
+import puppeteer from "puppeteer";
+
+export const create = async projectName => {
+  console.log(projectName);
+};
