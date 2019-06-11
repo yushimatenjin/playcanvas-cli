12d42650ffe80fcc3ec014fea225ade52d35bc7a
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2019-06-11 12:17:29 +0900

    update

---
+++ scripts/project-init.js
@@ -0,0 +1,26 @@
+import spawn from "cross-spawn";
+import path from "path";
+import fs from "fs-extra";
+
+export const projectInit = (projectName, settingsJson) => {
+  const template = "hot-reload";
+  const templatePath = path.join(__dirname, "..", "template", template);
+  const distPath = path.join(__dirname, template);
+
+  if (!fs.existsSync(distPath)) {
+    const settingsFilePath = path.join(distPath, "settings.json");
+    fs.copySync(templatePath, distPath);
+    fs.writeFileSync(settingsFilePath, JSON.stringify(settingsJson), "utf8");
+    spawn("npm", ["install", "--prefix", distPath], {
+      stdio: "inherit"
+    });
+  } else {
+    console.log("This project already exists");
+  }
+};
+
+// [x] フォルダをプログラムの置いてあるファイルへ作成
+// [x] settings.jsonを設置
+
+// [] フォルダをプログラムを実行した場所へ作成
+// Firebasetools
