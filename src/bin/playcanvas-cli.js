831813d1fed6c746f4d93698f238e14e63f4ae16
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2019-06-11 15:18:00 +0900

    update

--- src/bin/playcanvas-cli.js
+++ src/bin/playcanvas-cli.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
-import '@babel/polyfill';
-import { create, init } from "../scripts/commands";
+import { create, init } from '../scripts/commands';
 const args = process.argv.slice(2);
 
-const scriptIndex = args.findIndex(x => x === "create" || x === "init");
+const scriptIndex = args.findIndex(x => x === 'create' || x === 'init');
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 switch (script) {
-  case "create": {
-    const projectName = args[1] ? args[1] : "my-app";
+  case 'create': {
+    const projectName = args[1] ? args[1] : 'my-app';
     create(projectName);
 
     break;
   }
-  case "init": {
+  case 'init': {
     init();
     break;
   }
