a90de15014b56bf25bf49fcf29f60da1212045ce
yushimatenjin <ryuto.haga@gmail.com>
2019-06-12 14:45:16 +0900

    s3file save to zip

--- src/scripts/donwload.js
+++ src/scripts/donwload.js
@@ -3,7 +3,8 @@ import fs from 'fs-extra';
 import spawn from 'cross-spawn';
 import inquirer from 'inquirer';
 import PlayCanvas from 'playcanvas-node';
-
+import { sleep } from './utils/sleep';
+import request from 'request';
 export const download = async () => {
   const options = fs.readJSONSync('./playcanvas.json');
 
@@ -26,9 +27,25 @@ export const download = async () => {
   ) {
     console.log('Please waiting');
     const playcanvas = new PlayCanvas(options);
-    const file = await playcanvas.downloadApp();
-    console.log(options);
-    console.log(file);
+    try {
+      const file = await playcanvas.downloadApp();
+
+      const jobId = file.id;
+      await sleep(5000);
+      const { data } = await playcanvas.getJob(jobId);
+      const { download_url } = data;
+      await request({ url: download_url, encoding: null }, function(
+        err,
+        resp,
+        body
+      ) {
+        if (err) throw err;
+        fs.writeFile(`${projectName}.zip`, body, function(err) {});
+      });
+    } catch (e) {
+      console.log(e);
+      console.log('Download is failed.');
+    }
   } else {
     console.log('Please, run "playcanvas-cli init"');
   }
