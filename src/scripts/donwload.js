998fdb4eea1aa3c536941a94be23dd40b72b9b19
yushimatenjin <ryuto.haga@gmail.com>
2019-06-12 13:04:45 +0900

    update

--- src/scripts/donwload.js
+++ src/scripts/donwload.js
@@ -5,42 +5,31 @@ import inquirer from 'inquirer';
 import PlayCanvas from 'playcanvas-node';
 
 export const download = async () => {
-  let accessToken = process.env.PC_API_KEY;
-  const questions = [
-    {
-      type: 'input',
-      name: 'projectId',
-      message: `What's your project id`,
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: `What's your project name`,
-      default: 'playcanvas-app',
-    },
-  ];
+  const options = fs.readJSONSync('./playcanvas.json');
 
-  if (!accessToken) {
-    const authenticate = {
-      type: 'input',
-      name: 'accessToken',
-      message: "What's your accessToken",
-    };
-
-    questions.unshift(authenticate);
-  }
-
-  const ans = await inquirer.prompt(questions);
-  const projectName = ans.name;
-  accessToken = accessToken || ans.accessToken;
-
-  const projectId = Number(ans.projectId);
-  const playcanvas = new PlayCanvas({
+  const {
     accessToken,
+    scenes,
     projectId,
+    branchId,
     projectName,
-  });
+    remotePath,
+  } = options;
 
-  const app = await playcanvas.listScenes();
-  console.log(app);
+  if (
+    accessToken ||
+    scenes ||
+    projectId ||
+    branchId ||
+    projectName ||
+    remotePath
+  ) {
+    console.log('Please waiting');
+    const playcanvas = new PlayCanvas(options);
+    const file = await playcanvas.downloadApp();
+    console.log(options);
+    console.log(file);
+  } else {
+    console.log('Please, run "playcanvas-cli init"');
+  }
 };
