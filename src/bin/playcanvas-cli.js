b740f32b0b13b5b57adbc99c9e4c7b919d4235bd
yushimatenjin <ryuto.haga@gmail.com>
2019-06-18 12:25:51 +0900

    コマンドラインから引数を取得する

--- src/bin/playcanvas-cli.js
+++ src/bin/playcanvas-cli.js
@@ -5,18 +5,48 @@ import meow from 'meow';
 const cli = meow(
   `
 	Usage
-	  $ foo <input>
+	  $ playcanvas-cli <input>
 
-	Options
-	  --rainbow, -r  Include a rainbow
+	Options(required)
+    --accessToken -t
+    --projectId -p
+    --scene -s
+    --branchId -b
+    --projectName -n 
+    --remotePath -r
 
-	Examples
-	  $ foo unicorns --rainbow
-	  🌈 unicorns 🌈
+  Examples
+    CircleCI
+    --------
+    $ playcanvas-cli --accessToken "$PLAYCANVAS_ACCESS_TOKEN" --projectId "$PLAYCANVAS_PROJECT_ID" --scene "$PLAYCANVAS_SCENE" --branchId "PLAYCANVAS_BRANCH_ID" --projectId "$PLAYCANVAS_PROJECT_ID" --remotePath "$PLAYCANVAS_REMOTE_PATH"
+    $ playcanvas-cli -t "$PLAYCANVAS_ACCESS_TOKEN" -p "$PLAYCANVAS_PROJECT_ID" -s "$PLAYCANVAS_SCENE" -b "PLAYCANVAS_BRANCH_ID" -n "$PLAYCANVAS_PROJECT_NAME" --r "$PLAYCANVAS_REMOTE_PATH"
+    --------
+    $ playcanvas-cli -t token -p projectId -s scene -b branchId -n projectName -r remotePath 
+
 `,
   {
     flags: {
-      rainbow: {
+      accessToken: {
+        type: 'string',
+        alias: 't',
+      },
+      projectId: {
+        type: 'string',
+        alias: 'p',
+      },
+      scene: {
+        type: 'string',
+        alias: 's',
+      },
+      branchId: {
+        type: 'string',
+        alias: 'b',
+      },
+      projectName: {
+        type: 'string',
+        alias: 'n',
+      },
+      remotePath: {
         type: 'string',
         alias: 'r',
       },
@@ -24,26 +54,27 @@ const cli = meow(
   }
 );
 
-console.log(cli.input[0], cli.flags);
+// const args = process.argv.slice(2);
 
-const args = process.argv.slice(2);
+// const scriptIndex = args.findIndex(
+//   x =>
+//     x === 'create' ||
+//     x === 'init' ||
+//     x === 'download' ||
+//     x === 'login' ||
+//     x === 'logout'
+// );
+// const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 
-const scriptIndex = args.findIndex(
-  x =>
-    x === 'create' ||
-    x === 'init' ||
-    x === 'download' ||
-    x === 'login' ||
-    x === 'logout'
-);
-const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+const script = cli.input[0];
+
 switch (script) {
   case 'create': {
     create();
     break;
   }
   case 'init': {
-    init();
+    init(cli.flags);
     break;
   }
   case 'download': {
