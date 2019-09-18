f12d8ec2dc2911a676effb80fd72c2cdfe0b6cef
yushimatenjin <ryuto.haga@gmail.com>
2019-09-18 13:49:54 +0900

    lint

--- src/scripts/init.ts
+++ src/scripts/init.ts
@@ -1,11 +1,9 @@
-import 'dotenv/config';
-import inquirer from 'inquirer';
-import PlayCanvas from 'playcanvas-node';
-import path from 'path';
-import fs from 'fs-extra';
-import {PlayCanvasOptions,Branch,Scene } from '../types/data'
-
-
+import "dotenv/config";
+import inquirer from "inquirer";
+import PlayCanvas from "playcanvas-node";
+import path from "path";
+import fs from "fs-extra";
+import { PlayCanvasOptions, Branch, Scene } from "../types/data";
 
 export const init = async (options?: PlayCanvasOptions | any) => {
   {
@@ -15,7 +13,7 @@ export const init = async (options?: PlayCanvasOptions | any) => {
       projectId,
       branchId,
       projectName,
-      remotePath,
+      remotePath
     } = options;
 
     if (
@@ -33,13 +31,13 @@ export const init = async (options?: PlayCanvasOptions | any) => {
         projectId: Number(projectId),
         branchId,
         projectName,
-        remotePath,
+        remotePath
       };
 
-      const distPath = path.join('.');
-      const settingsFilePath = path.join(distPath, 'playcanvas.json');
-      fs.writeFileSync(settingsFilePath, JSON.stringify(settings), 'utf8');
-      console.log('created', `${distPath} > playcanvas.json`);
+      const distPath = path.join(".");
+      const settingsFilePath = path.join(distPath, "playcanvas.json");
+      fs.writeFileSync(settingsFilePath, JSON.stringify(settings), "utf8");
+      console.log("created", `${distPath} > playcanvas.json`);
       return;
     }
   }
@@ -48,45 +46,45 @@ export const init = async (options?: PlayCanvasOptions | any) => {
 
     const questions = [
       {
-        type: 'input',
-        name: 'projectId',
-        message: `What's your project id https://playcanvas.com/project/`,
-      },
+        type: "input",
+        name: "projectId",
+        message: `What's your project id https://playcanvas.com/project/`
+      }
     ];
 
     if (!accessToken) {
       const authenticate = {
-        type: 'input',
-        name: 'accessToken',
-        message: "What's your accessToken",
+        type: "input",
+        name: "accessToken",
+        message: "What's your accessToken"
       };
 
       questions.unshift(authenticate);
     } else {
     }
     const ans = await inquirer.prompt(questions);
-    accessToken = accessToken || ans.accessToken as string;
+    accessToken = accessToken || (ans.accessToken as string);
     const projectId = ans.projectId as number;
     const playcanvas = new PlayCanvas({
       accessToken,
-      projectId,
+      projectId
     });
 
     const branches = await playcanvas.listBranches();
-    const branchChoices = branches.map((branch:Branch) => {
+    const branchChoices = branches.map((branch: Branch) => {
       const { id, name } = branch;
       return {
         value: id,
-        name: `${name} | ${id}`,
+        name: `${name} | ${id}`
       };
     });
     const branchAnswer = await inquirer.prompt([
       {
-        type: 'list',
-        name: 'selectedBranch',
-        message: 'Please select a branch.',
-        choices: branchChoices,
-      },
+        type: "list",
+        name: "selectedBranch",
+        message: "Please select a branch.",
+        choices: branchChoices
+      }
     ]);
     const branchId = branchAnswer.selectedBranch;
     const playcanvas2 = new PlayCanvas({ accessToken, projectId, branchId });
@@ -97,17 +95,17 @@ export const init = async (options?: PlayCanvasOptions | any) => {
       const { id, name } = scene;
       return {
         name: `${name} | ${id} `,
-        value: id,
+        value: id
       };
     });
 
     const sceneAnswer = await inquirer.prompt([
       {
-        type: 'list',
-        name: 'selectedScenes',
-        message: 'Please select a scence.',
-        choices: scenesChoices,
-      },
+        type: "list",
+        name: "selectedScenes",
+        message: "Please select a scence.",
+        choices: scenesChoices
+      }
     ]);
 
     const scenes = sceneAnswer.selectedScenes;
@@ -115,7 +113,7 @@ export const init = async (options?: PlayCanvasOptions | any) => {
       accessToken,
       projectId,
       branchId,
-      scenes,
+      scenes
     });
 
     let remoteProjectName;
@@ -123,21 +121,21 @@ export const init = async (options?: PlayCanvasOptions | any) => {
       const pn = await playcanvas3.getProjectApp();
       remoteProjectName = pn[0].name;
     } catch (e) {
-      remoteProjectName = 'my-app';
+      remoteProjectName = "my-app";
     }
     const projectNameAnswer = await inquirer.prompt([
       {
-        type: 'input',
-        name: 'inputProjectName',
-        message: 'Please input a projectName',
-        default: remoteProjectName,
+        type: "input",
+        name: "inputProjectName",
+        message: "Please input a projectName",
+        default: remoteProjectName
       },
       {
-        type: 'input',
-        name: 'remotePath',
-        message: '',
-        default: 'dev',
-      },
+        type: "input",
+        name: "remotePath",
+        message: "",
+        default: "dev"
+      }
     ]);
     const projectName = projectNameAnswer.inputProjectName;
     const remotePath = projectNameAnswer.remotePath;
@@ -148,12 +146,12 @@ export const init = async (options?: PlayCanvasOptions | any) => {
       projectId: Number(projectId),
       branchId,
       projectName,
-      remotePath,
+      remotePath
     };
-    const distPath = path.join('.');
-    const settingsFilePath = path.join(distPath, 'playcanvas.json');
-    fs.writeFileSync(settingsFilePath, JSON.stringify(settingsJson), 'utf8');
-    console.log('created', `${distPath} > playcanvas.json`);
+    const distPath = path.join(".");
+    const settingsFilePath = path.join(distPath, "playcanvas.json");
+    fs.writeFileSync(settingsFilePath, JSON.stringify(settingsJson), "utf8");
+    console.log("created", `${distPath} > playcanvas.json`);
     return;
   } catch (e) {
     console.log(e);
