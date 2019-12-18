56b9f6a783c44725e2c37683bc5480d5d7926fd9
yushimatenjin <ryuto.haga@gmail.com>
2019-12-18 17:49:02 +0900

    Fixed bugs

---
+++ src/scripts/helpers/readdir.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+const readdirRecursively = (dir: string, files: Array<string> = [""]) => {
+  const dirents: fs.Dirent[] = fs.readdirSync(dir, { withFileTypes: true });
+  const dirs = [];
+  for (const dirent of dirents) {
+    if (dirent.isDirectory()) dirs.push(`${dir}/${dirent.name}`);
+    if (dirent.isFile()) {
+      const filePath = `${dir}/${dirent.name}`
+      files.push(filePath);
+    }
+  }
+  for (const d of dirs) {
+    files = readdirRecursively(d, files);
+  }
+  return files;
+};
+export { readdirRecursively }  
--- src/scripts/pwa.ts
+++
@@ -1,4 +0,0 @@
-import { sw } from "./sw";
-export async function pwa(name: string) {
-    sw(name)
-}
