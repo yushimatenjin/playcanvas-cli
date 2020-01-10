2388e368fe145a162611770559be179ef52bb78b
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2020-01-10 12:08:43 +0900

    不要なスクリプトを削除

--- src/scripts/create.ts
+++ src/scripts/create.ts
@@ -1,118 +1,13 @@
 import "dotenv/config";
-import inquirer from "inquirer";
-import PlayCanvas from "playcanvas-node";
 import { projectInit } from "./project-create";
-import { Branch, Scene } from "../types/data";
+import path from "path";
 
 export const create = async () => {
   try {
-    let accessToken = process.env.PC_API_KEY;
 
-    const questions = [
-      {
-        type: "input",
-        name: "projectId",
-        message: `What's your project id https://playcanvas.com/project/`
-      }
-    ];
+    const options = require(path.join(process.cwd(), "./", "playcanvas.json"));
 
-    if (!accessToken) {
-      const authenticate = {
-        type: "input",
-        name: "accessToken",
-        message: "What's your accessToken"
-      };
-
-      questions.unshift(authenticate);
-    }
-    const ans = await inquirer.prompt(questions);
-    accessToken = accessToken || (ans.accessToken as string);
-    const projectId = ans.projectId as number;
-    const playcanvas = new PlayCanvas({
-      accessToken,
-      projectId
-    });
-
-    const branches = await playcanvas.listBranches();
-    const branchChoices = branches.map((branch: Branch) => {
-      const { id, name } = branch;
-      return {
-        value: id,
-        name: `${name} | ${id}`
-      };
-    });
-    const branchAnswer = await inquirer.prompt([
-      {
-        type: "list",
-        name: "selectedBranch",
-        message: "Please select use branche.",
-        choices: branchChoices
-      }
-    ]);
-    const branchId = branchAnswer.selectedBranch;
-    const playcanvas2 = new PlayCanvas({ accessToken, projectId, branchId });
-
-    const remoteSecnes = await playcanvas2.listScenes();
-
-    const scenesChoices = remoteSecnes.map((scene: Scene) => {
-      const { id, name } = scene;
-      return {
-        name: `${name} | ${id} `,
-        value: id
-      };
-    });
-
-    const sceneAnswer = await inquirer.prompt([
-      {
-        type: "list",
-        name: "selectedScenes",
-        message: "Please select use scenes.",
-        choices: scenesChoices
-      }
-    ]);
-
-    const scenes = sceneAnswer.selectedScenes;
-    const playcanvas3 = new PlayCanvas({
-      accessToken,
-      projectId,
-      branchId,
-      scenes
-    });
-
-    let remoteProjectName;
-    try {
-      const pn = await playcanvas3.getProjectApp();
-      remoteProjectName = pn[0].name;
-    } catch (e) {
-      remoteProjectName = "my-app";
-    }
-    const projectNameAnswer = await inquirer.prompt([
-      {
-        type: "input",
-        name: "inputProjectName",
-        message: "Please input projectName",
-        default: remoteProjectName
-      },
-      {
-        type: "input",
-        name: "remotePath",
-        message: "",
-        default: "dev"
-      }
-    ]);
-    const projectName = projectNameAnswer.inputProjectName;
-    const remotePath = projectNameAnswer.remotePath;
-
-    const settingsJson = {
-      accessToken,
-      scenes: [scenes],
-      projectId,
-      branchId,
-      projectName,
-      remotePath
-    };
-
-    projectInit(projectName, settingsJson);
+    projectInit(options.projectName, options);
   } catch (e) {
     console.log("Not found");
   }
